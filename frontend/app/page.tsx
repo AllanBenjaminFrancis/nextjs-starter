06f5c27504e7d854b09ccaf487e6234049cfa2f0
Allan <allan@aestheticmedium.com>
2024-09-22 09:30:10 -0500

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
-import {FC} from "react";
+import { FC } from "react";
 
 export default async function Home() {
   return (
     <section className="flex w-full flex-col justify-center items-center">
       <h1 className="text-5xl mb-20">Next.js + Encore Web App Starter</h1>
+      <p>Hey friends.</p>
 
       <div className="grid grid-cols-4 max-w-6xl">
         <Card
@@ -34,9 +35,24 @@ export default async function Home() {
   );
 }
 
-const Card: FC<{ href: string; heading: string; desc: string }> = ({href, heading, desc}) => {
-  return <a href={href} className="group p-4 bg-white bg-opacity-0 transition hover:bg-black hover:bg-opacity-5" target="_blank">
-    <h2 className="mb-2 font-semibold">{heading} <span className="inline-block transition group-hover:translate-x-2">-&gt;</span></h2>
-    <p className="opacity-60">{desc}</p>
-  </a>
-}
+const Card: FC<{ href: string; heading: string; desc: string }> = ({
+  href,
+  heading,
+  desc,
+}) => {
+  return (
+    <a
+      href={href}
+      className="group p-4 bg-white bg-opacity-0 transition hover:bg-black hover:bg-opacity-5"
+      target="_blank"
+    >
+      <h2 className="mb-2 font-semibold">
+        {heading}{" "}
+        <span className="inline-block transition group-hover:translate-x-2">
+          -&gt;
+        </span>
+      </h2>
+      <p className="opacity-60">{desc}</p>
+    </a>
+  );
+};
